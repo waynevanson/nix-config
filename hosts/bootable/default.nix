e846e209d950c55600df165cfd5abccb1ea880cc
Wayne Van Son <waynevanson@gmail.com>
2026-02-22 08:44:11 +1100

    build: remove latest kernel from bootable image

--- hosts/bootable/default.nix
+++ hosts/bootable/default.nix
@@ -8,9 +8,6 @@
     "${modulesPath}/installer/cd-dvd/channel.nix"
   ];
 
-  # use the latest Linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
   # Enable SSH in boot process
   systemd.services.sshd.wantedBy = pkgs.lib.mkForce ["multi-user.target"];
   users.users.root.openssh.authorizedKeys.keys = [
