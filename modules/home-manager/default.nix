be7b55c2f0ad00d908e250607e64e7622a8c723d
Wayne Van Son <waynevanson@gmail.com>
2026-03-20 09:14:27 +1100

    refactor: remove alacritty

--- modules/home-manager/default.nix
+++
@@ -1,5 +0,0 @@
-# Nix module
-{...}: {
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-}
--- modules/home-manager/home/alacritty/default.nix
+++
@@ -1,8 +0,0 @@
-# todo: how to make alacritty read the system dark mode settings?
-{...}: {
-  programs.alacritty = {
-    enable = true;
-    theme = "catpuccin-mocha";
-    settings = {};
-  };
-}
