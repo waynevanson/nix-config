ee56f9f22bacd209c127f68c9e68f05b50829f07
Wayne Van Son <waynevanson@gmail.com>
2026-05-17 15:47:25 +1000

    Update default.nix

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,11 +1,32 @@
-{ ... }:
+{ config, ... }:
 
 let
+  sops' = {
+    sops.secrets.spaceship-token = {
+      sopsFile = ../../.sops.secrets.yaml;
+      key = "spaceship/token";
+    };
+  };
+
+  acme' = {
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "waynevanson@gmail.com";
+      certs."waynevanson.com" = {
+        dnsProvider = "spaceship";
+        credentialFiles = {
+          "SPACESHIP_API_TOKEN_FILE" = config.sops.secrets.spaceship-token.path;
+        };
+      };
+    };
+  };
+
   nginx' = {
     services.nginx = {
       enable = true;
-      virtualHosts."localhost" = {
-        default = true;
+      virtualHosts."waynevanson.com" = {
+        enableACME = true;
+        forceSSL = true;
         locations."/" = {
           return = "200 'Hello from NixOS server'";
         };
@@ -26,6 +47,8 @@ let
 in
 {
   imports = [
+    sops'
+    acme'
     nginx'
     ssh'
   ];
