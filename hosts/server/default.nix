33d6454c15d1715a72c867a4babfbb329e29fd77
Wayne Van Son <waynevanson@gmail.com>
2026-05-17 15:45:49 +1000

    server baby

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,4 +1,32 @@
 { ... }:
-{
 
+let
+  nginx' = {
+    services.nginx = {
+      enable = true;
+      virtualHosts."localhost" = {
+        default = true;
+        locations."/" = {
+          return = "200 'Hello from NixOS server'";
+        };
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [
+      80
+      443
+    ];
+  };
+
+  ssh' = {
+    services.ssh = {
+      enable = true;
+    };
+  };
+in
+{
+  imports = [
+    nginx'
+    ssh'
+  ];
 }
