6871afae1c5fa12a3e9f0b3ee7cbfd2ebc3d1a16
Wayne Van Son <waynevanson@gmail.com>
2026-05-18 10:51:29 +1000

    feat: add timezone for server

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -248,6 +248,24 @@ let
 
     system.stateVersion = "26.05";
     sops.defaultSopsFile = ../../.sops.secrets.yaml;
+
+    # Set your time zone.
+    time.timeZone = "Australia/Melbourne";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_AU.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_AU.UTF-8";
+      LC_IDENTIFICATION = "en_AU.UTF-8";
+      LC_MEASUREMENT = "en_AU.UTF-8";
+      LC_MONETARY = "en_AU.UTF-8";
+      LC_NAME = "en_AU.UTF-8";
+      LC_NUMERIC = "en_AU.UTF-8";
+      LC_PAPER = "en_AU.UTF-8";
+      LC_TELEPHONE = "en_AU.UTF-8";
+      LC_TIME = "en_AU.UTF-8";
+    };
   };
 
   ssh' = {
