4f39db983a4d08dfa1e14fd540b054c8c2cd7748
Wayne Van Son <waynevanson@gmail.com>
2026-06-15 15:30:40 +1000

    auto attach tmux

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -2,5 +2,11 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+
+    initExtra = ''
+      if [[ -z "$TMUX" && "$TTY" == /dev/tty* ]]; then
+        exec tmux new-session -A -s main
+      fi
+    '';
   };
 }
