2f563b8ca3a0d31485a4456467cdc3f8079380fd
Wayne Van Son <waynevanson@gmail.com>
2026-06-15 12:56:05 +1000

    add kmscon colors

--- hosts/writer/default.nix
+++ hosts/writer/default.nix
@@ -1,10 +1,32 @@
 {
   pkgs,
-  inputs,
-  system,
   ...
 }:
 let
+
+  catppuccinMocha = {
+    palette = "custom";
+
+    palette-black = "30,30,46";
+    palette-red = "243,139,168";
+    palette-green = "166,227,161";
+    palette-yellow = "249,226,175";
+    palette-blue = "137,180,250";
+    palette-magenta = "245,194,231";
+    palette-cyan = "148,226,213";
+    palette-light-grey = "186,194,222";
+    palette-dark-grey = "88,91,112";
+    palette-light-red = "243,139,168";
+    palette-light-green = "166,227,161";
+    palette-light-yellow = "249,226,175";
+    palette-light-blue = "137,180,250";
+    palette-light-magenta = "245,194,231";
+    palette-light-cyan = "148,226,213";
+    palette-white = "166,173,200";
+
+    palette-foreground = "205,214,244";
+    palette-background = "30,30,46";
+  };
 
   system' = {
     time.timeZone = "Australia/Melbourne";
@@ -81,7 +103,7 @@ let
       enable = true;
       config = {
         font-name = "JetBrains Mono";
-      };
+      } // catppuccinMocha;
     };
 
     fonts.packages = [
