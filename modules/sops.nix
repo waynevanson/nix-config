e714931d15d94c65bfc3d1226ebfa766c485a4bf
Wayne Van Son <waynevanson@gmail.com>
2026-06-21 11:36:48 +1000

    agist

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,11 +1,12 @@
 # bind the local sops secrets so they can be reused
 # We share all secrets across all systems so all good
-{ ... }:
+{ lib, ... }:
 {
   services.openssh.enable = true;
 
   sops = {
     defaultSopsFile = ../.sops.secrets.yaml;
+    age.sshKeyPaths = lib.mkDefault [ ];
     secrets = {
       spaceship-client-id.key = "spaceship/client-id";
       spaceship-client-secret.key = "spaceship/client-secret";
