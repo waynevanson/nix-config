26b4bf49112a55c7a174fa73dbb41cc5d817591c
Wayne Van Son <waynevanson@gmail.com>
2026-06-24 12:26:35 +1000

    refactor(pi): import extensions from packages instead of vendoring
    - Add packages/codelens fetched from ex-git/codeLens v2.2.2
    - Wire codelens from the new package
    - Wire qna and questionnaire from the pi-coding-agent package examples
    - Remove vendored extension copies

---
+++ packages/codelens/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildNpmPackage,
+  fetchFromGitHub,
+  python3,
+  gnumake,
+  gcc,
+}:
+
+buildNpmPackage {
+  pname = "codelens";
+  version = "2.2.2";
+
+  src = fetchFromGitHub {
+    owner = "ex-git";
+    repo = "codeLens";
+    rev = "v2.2.2";
+    hash = "sha256-M9+7utcpboGzvy01iXK80Ft0Q4ZzSzcVga/EuDrd0LY=";
+  };
+
+  npmDepsHash = "sha256-3M8Z1znoKbgla6UEyw8m5DUoskVVhtgrHEYNIEv+ePQ=";
+
+  nativeBuildInputs = [
+    python3
+    gnumake
+    gcc
+  ];
+
+  env.CXXFLAGS = "-std=c++20";
+
+  meta = {
+    description = "CodeLens code context index server";
+    homepage = "https://github.com/ex-git/codeLens";
+    license = lib.licenses.mit;
+  };
+}
