f38401fc0f59f7b628a88e19dd90f164d2539be7
Wayne Van Son <waynevanson@gmail.com>
2026-07-03 16:09:43 +1000

    refactor

--- hosts/server/web.nix
+++ hosts/server/web.nix
@@ -15,37 +15,8 @@
         "SPACESHIP_API_SECRET_FILE" = config.sops.secrets.spaceship-client-secret.path;
       };
     };
-    certs."procurare.tech" = {
-      group = "nginx";
-      dnsProvider = "digitalocean";
-      webroot = null;
-      reloadServices = [ "nginx.service" ];
-      extraLegoFlags = [ "--dns.propagation-disable-ans" ];
-      credentialFiles = {
-        "DO_AUTH_TOKEN_FILE" = config.sops.secrets.digitalocean-token.path;
-      };
-    };
   };
 
-  services.nginx = {
-    enable = true;
-    virtualHosts = {
-      "procurare.tech" = {
-        useACMEHost = "procurare.tech";
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://10.233.1.2";
-          proxyWebsockets = true;
-          recommendedProxySettings = true;
-        };
-      };
-    };
-  };
-
-  networking.extraHosts = ''
-    127.0.0.1 procurare.tech
-    127.0.0.1 www.procurare.tech
-  '';
   networking.firewall.allowedTCPPorts = [
     80
     443
