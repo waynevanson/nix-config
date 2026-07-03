79d8eb1fb9b085451c052d68f49dab4fdc534bae
Wayne Van Son <waynevanson@gmail.com>
2026-07-03 20:12:00 +1000

    disable container for now

--- hosts/server/procurare.nix
+++ hosts/server/procurare.nix
@@ -32,31 +32,31 @@
     127.0.0.1 www.procurare.tech
   '';
 
-  containers.procurare = {
-    autoStart = true;
-    privateNetwork = true;
-    hostAddress = "10.233.1.1";
-    localAddress = "10.233.1.2";
-    config =
-      {
-        pkgs,
-        ...
-      }:
-      {
-        system.stateVersion = "26.05";
+  # containers.procurare = {
+  #   autoStart = true;
+  #   privateNetwork = true;
+  #   hostAddress = "10.233.1.1";
+  #   localAddress = "10.233.1.2";
+  #   config =
+  #     {
+  #       pkgs,
+  #       ...
+  #     }:
+  #     {
+  #       system.stateVersion = "26.05";
 
-        networking.firewall.allowedTCPPorts = [ 80 ];
+  #       networking.firewall.allowedTCPPorts = [ 80 ];
 
-        services.nginx = {
-          enable = true;
-          virtualHosts."procurare" = {
-            default = true;
-            root = pkgs.writeTextDir "index.html" "Hello, world!";
-            locations."/" = {
-              index = "index.html";
-            };
-          };
-        };
-      };
-  };
+  #       services.nginx = {
+  #         enable = true;
+  #         virtualHosts."procurare" = {
+  #           default = true;
+  #           root = pkgs.writeTextDir "index.html" "Hello, world!";
+  #           locations."/" = {
+  #             index = "index.html";
+  #           };
+  #         };
+  #       };
+  #     };
+  # };
 }
