5f2ef6bea45ec24b46154f0fa01086f649f721d4
Wayne Van Son <waynevanson@gmail.com>
2026-07-07 15:35:58 +1000

    yeah remove attic conf

--- modules/nix.nix
+++ modules/nix.nix
@@ -5,12 +5,10 @@
       "flakes"
     ];
     substituters = [
-      "https://atticd.waynevanson.com/default"
       "https://nix-community.cachix.org"
       "https://cache.nixos.org/"
     ];
     trusted-public-keys = [
-      "default:pS97iTPm4yOtVXO7lpAINY+vJB5tTEJqAmPJQPWajr0="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     trusted-users = [
